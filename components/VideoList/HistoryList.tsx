7d1b6a10419a852e8cd854a50e46cbe985418553
Joey <786678257@qq.com>
2023-02-04 17:44:00 +0800

    feat: histtory empty

--- components/VideoList/HistoryList.tsx
+++ components/VideoList/HistoryList.tsx
@@ -1,3 +1,4 @@
+import { Empty } from 'antd'
 import React, { memo } from 'react'
 import { useHistory } from '../../context/HistoryContext'
 import VideoCard from './VideoCard'
@@ -8,19 +9,22 @@ interface Props {
 
 const HistoryList: React.FC<Props> = ({ count }) => {
   const { histories } = useHistory()
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-4  lg:grid-cols-6 xl:grid-cols-8 gap-2">
-      {count == null
-        ? histories
-            .slice(0, count)
-            .map((video) => (
+  if (histories.length > 0) {
+    return (
+      <div className="grid grid-cols-2 md:grid-cols-4  lg:grid-cols-6 xl:grid-cols-8 gap-2">
+        {count == null
+          ? histories
+              .slice(0, count)
+              .map((video) => (
+                <VideoCard key={video.vod_id} video={video} isHistory />
+              ))
+          : histories.map((video) => (
               <VideoCard key={video.vod_id} video={video} isHistory />
-            ))
-        : histories.map((video) => (
-            <VideoCard key={video.vod_id} video={video} isHistory />
-          ))}
-    </div>
-  )
+            ))}
+      </div>
+    )
+  }
+  return <Empty />
 }
 
 export default memo(HistoryList)
