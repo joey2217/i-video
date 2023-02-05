342c842010b701f00311c7a9a6fdda548ac3d631
Joey <786678257@qq.com>
2023-02-05 18:18:28 +0800

    style: xs style

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,78 +6,39 @@ import ActiveLink from './ActiveLink'
 
 const Header: React.FC = () => {
   const router = useRouter()
-  const [show, setShow] = useState(false)
   const [value, setValue] = useState('')
   const onSearch = (val: string) => {
     console.log(val)
     setValue('')
-    setShow(false)
     router.push(`/search?q=${val}`)
   }
 
   return (
     <header
-      className="bg-gray-900 hover:shadow-md sticky top-0 z-50 px-0 md:px-2 lg:px-4"
+      className="bg-gray-900 hover:shadow-md sticky top-0 z-50 px-2 lg:px-4"
       id="header"
     >
-      <div className="md:flex h-12">
-        <div className="flex items-center justify-between h-full flex-shrink-0">
+      <div className="md:flex flex-col md:flex-row items-center">
+        <div className="flex items-center justify-between h-10 md:h-12 flex-shrink-0">
           <Link href="/">
             <a className="text-lg text-gray-800  transition-colors duration-200 transform dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
               视频资源网
             </a>
           </Link>
 
-          <div
-            className="flex items-center justify-center md:hidden cursor-pointer w-8 h-8 "
-            onClick={() => setShow((s) => !s)}
-          >
-            {show ? (
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 48 48"
-                fill="currentColor"
-                stroke="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8 8L40 40"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M8 40L40 8"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
+          <div className="flex-1 max-w-xs ml-4 flex md:hidden items-center justify-center">
+            <Input.Search
+              placeholder="输入关键词"
+              value={value}
+              allowClear
+              onChange={(e) => setValue(e.target.value)}
+              onSearch={onSearch}
+              enterButton
+            />
           </div>
         </div>
-
-        <div
-          className={`${
-            show ? 'flex' : 'hidden'
-          }  bg-gray-900 md:flex md:items-center md:justify-between`}
-          onClick={() => show && setShow(false)}
-        >
-          <nav className="w-full flex flex-col px-2 py-3 md:flex-row md:mx-0 md:py-0">
+        <div className="flex flex-1 items-center bg-gray-900 h-10 md:h-12">
+          <nav className="w-full flex justify-between md:justify-start md:px-2 md:py-3">
             <ActiveLink href="/">
               <a className="link">首页</a>
             </ActiveLink>
@@ -103,20 +64,20 @@ const Header: React.FC = () => {
               <a className="link">看过</a>
             </ActiveLink>
           </nav>
-        </div>
-        <div
-          className={`${show ? 'flex pb-4 px-1' : 'hidden'} ${
-            router.pathname === '/search' ? 'hidden' : 'md:flex'
-          } md:w-56 lg:w-80 items-center ml-auto bg-gray-900`}
-        >
-          <Input.Search
-            placeholder="输入关键词"
-            value={value}
-            allowClear
-            onChange={(e) => setValue(e.target.value)}
-            onSearch={onSearch}
-            enterButton
-          />
+          <div
+            className={`hidden md:flex px-1 ${
+              router.pathname === '/search' ? 'hidden' : 'md:flex'
+            } w-80 lg:w-[500px] items-center bg-gray-900`}
+          >
+            <Input.Search
+              placeholder="输入关键词"
+              value={value}
+              allowClear
+              onChange={(e) => setValue(e.target.value)}
+              onSearch={onSearch}
+              enterButton
+            />
+          </div>
         </div>
       </div>
     </header>
